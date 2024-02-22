4de3ee75a1560db72194dfa51f8225a81df3d0bc
idaira <idairaconejillo@gmail.com>
2024-02-22 15:42:47 +0100

    palante

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const contenedorPersonal = document.querySelector("#personal ul")
+const contenedorPersonal = document.querySelector("#estudios ul")
+const contenedorPersonal = document.querySelector("#experiencia ul")
+const contenedorPersonal = document.querySelector("#skills ul")
+
 fetch("amarillo.json")
 .then(resp => resp.json())
-.then(data => console.log(data))
+.then(data => crearCV(data))
+
+function crearCV(cv){
+    console.log(cv)
+}
